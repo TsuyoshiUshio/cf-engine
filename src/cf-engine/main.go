0010ee34fa21971591c1b3b7639f2734cd7a94b5
sakkuru <sakkuru0323@gmail.com>
2017-04-06 15:10:56 +0900

    add createStrageAccount function

--- src/cf-engine/main.go
+++ src/cf-engine/main.go
@@ -1,113 +1,106 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"io/ioutil"
-	"flag"
-    "gopkg.in/yaml.v2"
-
 
 	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
-	"github.com/Azure/azure-sdk-for-go/arm/storage"
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	"github.com/Azure/azure-sdk-for-go/arm/storage"
 	storagem "github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/to"
 )
 
 func main() {
-    fileName = flag.String("f", ".container-config.yml", "Configuration File: Path to Configuration file")
+	fileName := flag.String("f", ".container-config.yml", "Configuration File: Path to Configuration file")
 	flag.Parse()
-	file, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Fatalf("Error: %v", err)		
-		return 
-	}
-	defer file.Close()
-	fileNameYaml = string(file)
-
-
-
+	fmt.Println(*fileName)
+	// file, err := ioutil.ReadFile(*fileName)
+	// if err != nil {
+	// 	log.Fatalf("Error: %v", err)
+	// 	return
+	// }
+	// defer file.Close()
+	// fileNameYaml = string(file)
 
 	resourceGroup := os.Getenv("RESOURCE_GROUP")
-	name   := os.Getenv("STORAGE_ACCOUNT_NAME")
+	name := os.Getenv("STORAGE_ACCOUNT_NAME")
 
-	c := map[string]string {
-		"AZURE_CLIENT_ID": os.Getenv("AZURE_CLIENT_ID"),
-		"AZURE_CLIENT_SECRET": os.Getenv("AZURE_CLIENT_SECRET"),
+	c := map[string]string{
+		"AZURE_CLIENT_ID":       os.Getenv("AZURE_CLIENT_ID"),
+		"AZURE_CLIENT_SECRET":   os.Getenv("AZURE_CLIENT_SECRET"),
 		"AZURE_SUBSCRIPTION_ID": os.Getenv("AZURE_SUBSCRIPTION_ID"),
-		"AZURE_TENANT_ID": os.Getenv("AZURE_TENANT_ID")}
+		"AZURE_TENANT_ID":       os.Getenv("AZURE_TENANT_ID")}
 	if err := checkEnvVar(&c); err != nil {
 		log.Fatalf("Error: %v", err)
 		return
-	}	
-
+	}
 
 	spt, err := helpers.NewServicePrincipalTokenFromCredentials(c, azure.PublicCloud.ResourceManagerEndpoint)
 	if err != nil {
 		log.Fatalf("Error: %v", err)
-		return 
+		return
 	}
 
-	gp := resources.Group {
+	gp := resources.Group{
 		Location: to.StringPtr("japaneast")}
 	gc := resources.NewGroupsClient(c["AZURE_SUBSCRIPTION_ID"])
 	gc.Authorizer = spt
 	_, err = gc.CreateOrUpdate(resourceGroup, gp)
 	if err != nil {
 		log.Fatalf("Error: %v", err)
-		return 
+		return
 	}
 
-	
 	ac := storage.NewAccountsClient(c["AZURE_SUBSCRIPTION_ID"])
 	ac.Authorizer = spt
 
-	cna, err := ac.CheckNameAvailability(
-		storage.AccountCheckNameAvailabilityParameters {
-			Name: to.StringPtr(name),
-			Type: to.StringPtr("Microsoft.Storage/storageAccounts")})
+	accountKey, err := createStrageAccount(ac, resourceGroup, name)
 
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-		return
-	}
+	// cna, err := ac.CheckNameAvailability(
+	// 	storage.AccountCheckNameAvailabilityParameters{
+	// 		Name: to.StringPtr(name),
+	// 		Type: to.StringPtr("Microsoft.Storage/storageAccounts")})
 
-	if !to.Bool(cna.NameAvailable) {
-		fmt.Printf("%s is unavailable -- try with another name\n", name)
-		return
-	}
-	fmt.Printf("%s is available\n\n", name)
+	// if err != nil {
+	// 	log.Fatalf("Error: %v", err)
+	// 	return
+	// }
 
-	cp := storage.AccountCreateParameters{
-		Sku: &storage.Sku{
-			Name: storage.StandardLRS,
-			Tier: storage.Standard},
-			Location: to.StringPtr("japaneast")}
-	cancel := make(chan struct{})
-	if _, err = ac.Create(resourceGroup, name, cp, cancel); err != nil {
-		fmt.Printf("Create '%s' storage account failed: %v\n", name, err)
+	// if !to.Bool(cna.NameAvailable) {
+	// 	fmt.Printf("%s is unavailable -- try with another name\n", name)
+	// 	return
+	// }
+	// fmt.Printf("%s is available\n\n", name)
 
-	}
+	// cp := storage.AccountCreateParameters{
+	// 	Sku: &storage.Sku{
+	// 		Name: storage.StandardLRS,
+	// 		Tier: storage.Standard},
+	// 	Location: to.StringPtr("japaneast")}
+	// cancel := make(chan struct{})
+	// if _, err = ac.Create(resourceGroup, name, cp, cancel); err != nil {
+	// 	fmt.Printf("Create '%s' storage account failed: %v\n", name, err)
 
-	keyResults, err := ac.ListKeys(resourceGroup, name)
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-		return
-	}
-	accountKeyList := keyResults.Keys
-	pl := *accountKeyList
-	accountKey := pl[0]
-	value   := accountKey.Value
-	fmt.Printf("AccountKey: %s\nValue: %s", name, *value)
+	// }
 
-	createContainer(name, *value, os.Getenv("CONTAINER_NAME"))
+	// keyResults, err := ac.ListKeys(resourceGroup, name)
+	// if err != nil {
+	// 	log.Fatalf("Error: %v", err)
+	// 	return
+	// }
+	// accountKeyList := keyResults.Keys
+	// pl := *accountKeyList
+	// accountKey := pl[0]
+	// value := accountKey.Value
+	fmt.Printf("AccountKey: %s\nValue: %s", name, accountKey)
 
+	createContainer(name, accountKey, os.Getenv("CONTAINER_NAME"))
 
 }
-
 
 func createContainer(storageAccountName string, storageAccountKeyValue string, containerName string) {
 	client, err := storagem.NewBasicClient(storageAccountName, storageAccountKeyValue)
@@ -127,7 +120,7 @@ func createContainer(storageAccountName string, storageAccountKeyValue string, c
 }
 
 func checkEnvVar(envVars *map[string]string) error {
-	var missingVars []string 
+	var missingVars []string
 	for varName, value := range *envVars {
 		if value == "" {
 			missingVars = append(missingVars, varName)
@@ -137,4 +130,50 @@ func checkEnvVar(envVars *map[string]string) error {
 		return fmt.Errorf("Missing environment variables %v", missingVars)
 	}
 	return nil
+}
+
+func createStrageAccount(ac storage.AccountsClient, resourceGroup string, name string) (string, error) {
+
+	cna, err := ac.CheckNameAvailability(
+		storage.AccountCheckNameAvailabilityParameters{
+			Name: to.StringPtr(name),
+			Type: to.StringPtr("Microsoft.Storage/storageAccounts")})
+
+	fmt.Println(cna)
+	fmt.Println(err)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+		return "", err
+	}
+
+	if !to.Bool(cna.NameAvailable) {
+		fmt.Printf("%s is unavailable -- try with another name\n", name)
+		return "", err
+	}
+	fmt.Printf("%s is available\n\n", name)
+
+	cp := storage.AccountCreateParameters{
+		Sku: &storage.Sku{
+			Name: storage.StandardLRS,
+			Tier: storage.Standard},
+		Location: to.StringPtr("japaneast")}
+	cancel := make(chan struct{})
+	if _, err = ac.Create(resourceGroup, name, cp, cancel); err != nil {
+		fmt.Printf("Create '%s' storage account failed: %v\n", name, err)
+
+	}
+
+	keyResults, err := ac.ListKeys(resourceGroup, name)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+		return "", err
+	}
+	accountKeyList := keyResults.Keys
+	pl := *accountKeyList
+	accountKey := pl[0]
+	value := accountKey.Value
+	fmt.Printf("AccountKey: %s\nValue: %s", name, *value)
+
+	return *value, nil
+
 }
