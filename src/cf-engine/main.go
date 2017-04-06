592f0460fa4070e5cd7e4b517ac39c10ae9364db
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-04-06 16:00:45 +0900

    Feature: Reading configuration from YAML

--- src/cf-engine/main.go
+++ src/cf-engine/main.go
@@ -4,7 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
+
+	"github.com/spf13/viper"
 
 	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
@@ -15,25 +16,24 @@ import (
 )
 
 func main() {
-	fileName := flag.String("f", ".container-config.yml", "Configuration File: Path to Configuration file")
+	fileName := flag.String("f", "config", "Configuration File: Path to Configuration file")
 	flag.Parse()
 	fmt.Println(*fileName)
-	// file, err := ioutil.ReadFile(*fileName)
-	// if err != nil {
-	// 	log.Fatalf("Error: %v", err)
-	// 	return
-	// }
-	// defer file.Close()
-	// fileNameYaml = string(file)
+	viper.SetConfigName(*fileName)
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error: config file %s", *fileName))
+	}
 
-	resourceGroup := os.Getenv("RESOURCE_GROUP")
-	name := os.Getenv("STORAGE_ACCOUNT_NAME")
+	resourceGroup := viper.Get("RESOURCE_GROUP").(string)
+	storageAccounts := viper.Get("STORAGE_ACCOUNTS")
 
 	c := map[string]string{
-		"AZURE_CLIENT_ID":       os.Getenv("AZURE_CLIENT_ID"),
-		"AZURE_CLIENT_SECRET":   os.Getenv("AZURE_CLIENT_SECRET"),
-		"AZURE_SUBSCRIPTION_ID": os.Getenv("AZURE_SUBSCRIPTION_ID"),
-		"AZURE_TENANT_ID":       os.Getenv("AZURE_TENANT_ID")}
+		"AZURE_CLIENT_ID":       viper.Get("AZURE_CLIENT_ID").(string),
+		"AZURE_CLIENT_SECRET":   viper.Get("AZURE_CLIENT_SECRET").(string),
+		"AZURE_SUBSCRIPTION_ID": viper.Get("AZURE_SUBSCRIPTION_ID").(string),
+		"AZURE_TENANT_ID":       viper.Get("AZURE_TENANT_ID").(string)}
 	if err := checkEnvVar(&c); err != nil {
 		log.Fatalf("Error: %v", err)
 		return
@@ -58,47 +58,19 @@ func main() {
 	ac := storage.NewAccountsClient(c["AZURE_SUBSCRIPTION_ID"])
 	ac.Authorizer = spt
 
-	accountKey, err := createStrageAccount(ac, resourceGroup, name)
-
-	// cna, err := ac.CheckNameAvailability(
-	// 	storage.AccountCheckNameAvailabilityParameters{
-	// 		Name: to.StringPtr(name),
-	// 		Type: to.StringPtr("Microsoft.Storage/storageAccounts")})
-
-	// if err != nil {
-	// 	log.Fatalf("Error: %v", err)
-	// 	return
-	// }
-
-	// if !to.Bool(cna.NameAvailable) {
-	// 	fmt.Printf("%s is unavailable -- try with another name\n", name)
-	// 	return
-	// }
-	// fmt.Printf("%s is available\n\n", name)
-
-	// cp := storage.AccountCreateParameters{
-	// 	Sku: &storage.Sku{
-	// 		Name: storage.StandardLRS,
-	// 		Tier: storage.Standard},
-	// 	Location: to.StringPtr("japaneast")}
-	// cancel := make(chan struct{})
-	// if _, err = ac.Create(resourceGroup, name, cp, cancel); err != nil {
-	// 	fmt.Printf("Create '%s' storage account failed: %v\n", name, err)
-
-	// }
-
-	// keyResults, err := ac.ListKeys(resourceGroup, name)
-	// if err != nil {
-	// 	log.Fatalf("Error: %v", err)
-	// 	return
-	// }
-	// accountKeyList := keyResults.Keys
-	// pl := *accountKeyList
-	// accountKey := pl[0]
-	// value := accountKey.Value
-	fmt.Printf("AccountKey: %s\nValue: %s", name, accountKey)
-
-	createContainer(name, accountKey, os.Getenv("CONTAINER_NAME"))
+	for _, account := range storageAccounts.([]interface{}) {
+		accountProperty := account.(map[interface{}]interface{})
+		accountKey, err := createStrageAccount(ac, resourceGroup, accountProperty["name"].(string))
+		if err != nil {
+			log.Fatalf("Storage Account Creation Error %v", err)
+			return
+		}
+		containers := accountProperty["containers"].([]interface{})
+		for _, container := range containers {
+			containerProperty := container.(map[interface{}]interface{})
+			createContainer(accountProperty["name"].(string), accountKey, containerProperty["name"].(string))
+		}
+	}
 
 }
 
